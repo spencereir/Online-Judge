de5407c44219a3f9f218ee0a6f0cc45d5f27069f
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-07-31 16:00:37 -0400

    Problems list formats languages.

--- public/js/problems.js
+++ public/js/problems.js
@@ -17,7 +17,7 @@ socket.on("problems-response", function(problemArr) {
     problemList.clear();
     // Insert it into the table
     problemArr.forEach(function(val, index, arr) {
-        problemList.add({ name: val.name, points: Math.floor(Math.random() * 100), partial: val.partial, languages: val.languages.join(", ")});
+        problemList.add({ name: val.name, points: Math.floor(Math.random() * 100), partial: val.partial, languages: convertLanguages(val.languages).join(", ")});
     });
 });
 
@@ -47,4 +47,13 @@ $("#problems-list-thead-points").click(function() {
     $("#problems-list-thead-name").find("span").toggleClass("glyphicon-triangle-bottom glyphicon-triangle-top");
 });
 
-// Convert 
+// HackerRank's languages list
+var hrLanguages = { "languages": { "names": { "c": "C", "cpp": "C++", "java": "Java", "csharp": "C#", "php": "PHP", "ruby": "Ruby", "python": "Python 2", "perl": "Perl", "haskell": "Haskell", "clojure": "Clojure", "scala": "Scala", "bash": "Bash", "lua": "Lua", "erlang": "Erlang", "javascript": "Javascript", "go": "Go", "d": "D", "ocaml": "OCaml", "pascal": "Pascal", "sbcl": "Common Lisp (SBCL)", "python3": "Python 3", "groovy": "Groovy", "objectivec": "Objective-C", "fsharp": "F#", "cobol": "COBOL", "visualbasic": "VB.NET", "lolcode": "LOLCODE", "smalltalk": "Smalltalk", "tcl": "Tcl", "whitespace": "Whitespace", "tsql": "T-SQL", "java8": "Java 8", "db2": "DB2", "octave": "Octave", "r": "R", "xquery": "XQuery", "racket": "Racket", "rust": "Rust", "fortran": "Fortran", "swift": "Swift", "oracle": "Oracle", "mysql": "MySQL" }, "codes": { "c": 1, "cpp": 2, "java": 3, "python": 5, "perl": 6, "php": 7, "ruby": 8, "csharp": 9, "mysql": 10, "oracle": 11, "haskell": 12, "clojure": 13, "bash": 14, "scala": 15, "erlang": 16, "lua": 18, "javascript": 20, "go": 21, "d": 22, "ocaml": 23, "r": 24, "pascal": 25, "sbcl": 26, "python3": 30, "groovy": 31, "objectivec": 32, "fsharp": 33, "cobol": 36, "visualbasic": 37, "lolcode": 38, "smalltalk": 39, "tcl": 40, "whitespace": 41, "tsql": 42, "java8": 43, "db2": 44, "octave": 46, "xquery": 48, "racket": 49, "rust": 50, "swift": 51, "fortran": 54 } } };
+
+// Converts languages list into a nice formatted one
+function convertLanguages(languages) {
+    languages.forEach(function (val, index, arr) {
+        arr[index] = hrLanguages.languages.names[val];
+    });
+    return languages;
+};
