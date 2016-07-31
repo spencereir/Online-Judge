954a51f8df1e97da57d18c1afa85b2e858f85ef4
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-07-31 00:04:19 -0400

    Create auth.js config file.

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+// Account authorization
+module.exports = {
+    "googleAuth": {
+        "clientID": "INSERT CLIENT ID",
+        "clientSecret": "INSERT CLIENT SECRET",
+        "callbackURL": "INSERT CALLBACK URL"
+    }
+};
