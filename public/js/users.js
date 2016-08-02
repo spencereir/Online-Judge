80e363a81da367cd2ccb0425874fc23725933c0e
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-08-02 06:44:51 -0400

    Added links to list of users.

--- public/js/users.js
+++ public/js/users.js
@@ -1,6 +1,6 @@
 // List options
 var options = {
-    valueNames: ["username", "name", "school", "points"]
+    valueNames: ["username", "name", "school", "points", { attr: "href", name: "namelink" }, { attr: "href", name: "usernamelink" }]
 };
 
 // Initialize the list
@@ -17,7 +17,14 @@ socket.on("users-response", function(userArr) {
     userList.clear();
     // Insert it into the table
     userArr.forEach(function(val, index, arr) {
-        userList.add({ username: val.username, name: val.name, school: val.school, points: val.points});
+        userList.add({ username: val.username, name: val.name, usernamelink: "/user?username=" + val.username, namelink: "/user?username=" + val.username, school: val.school, points: val.points});
+    });
+    // Loop through table rows turning names into links
+    $("#users-table-tbody > tr > td.username.usernamelink").each(function() {
+        $(this).html('<a href="' + $(this).attr("href") + '">' + $(this).text() + "</a>");
+    });
+    $("#users-table-tbody > tr > td.name.namelink").each(function() {
+        $(this).html('<a href="' + $(this).attr("href") + '">' + $(this).text() + "</a>");
     });
 });
 
